b41ad57472e8dc8ebd19164e41946b3703a3ecd3
오영재 <dhdnqkrtk22@naver.com>
2023-05-19 15:56:03 +0900

    add community

--- front-end/src/views/CommunityView.vue
+++ front-end/src/views/CommunityView.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <Home01Navbar/>
+    <div class="d-flex justify-content-end">
+      <router-link :to="{ name: 'CreateArticleView' }">게시물 작성</router-link>
+    </div>
+    <Community01ArticleList />
   </div>
 </template>
 
 <script>
-export default {
+import Home01Navbar from '@/components/HOME/Home01Navbar.vue'
+import Community01ArticleList from '@/components/COMMUNITY/Community01ArticleList.vue'
 
+export default {
+  name: 'CommunityView',
+  components: {
+    Home01Navbar,
+    Community01ArticleList
+  },
+  computed:{
+    isLogin() {
+      return this.$store.getters.isLogin // 로그인 여부
+    }
+  },
+  created() {
+    this.getArticles()
+  },
+  methods: {
+    getArticles() {
+      if (this.isLogin) {
+        this.$store.dispatch('getArticles')
+      } else {
+        alert('로그인이 필요한 페이지입니다...')
+      }
+      // 로그인이 되어 있으면 getArticles action 실행
+      // 로그인 X라면 login 페이지로 이동
+    }
+  }
 }
 </script>
 
