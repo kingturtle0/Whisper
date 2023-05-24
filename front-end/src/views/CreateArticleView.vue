ec70ba3e73356ad8ac76c976a7ca8619b291cd53
sionyy <sion941004@naver.com>
2023-05-24 14:09:32 +0900

    0524 오후

--- front-end/src/views/CreateArticleView.vue
+++ front-end/src/views/CreateArticleView.vue
@@ -1,64 +1,213 @@
 <template>
   <div class="container">
-    <Home01Navbar/>
-    <h1>게시글 작성</h1>
-    <form @submit.prevent="createArticle">
-      <label for="title">제목 : </label>
-      <input type="text" id="title" v-model.trim="title"><br>
-      <label for="content">내용 : </label>
-      <textarea id="content" cols="30" rows="10" v-model="content"></textarea><br>
-      <input type="submit" id="submit">
-    </form>
+    <Home01Navbar />
+    <div class="form-container">
+      <div class="">
+      <div class='first'>
+        <h3>
+          <i class="fas fa-pen fa-sm"></i> 글 작성
+        </h3>
+      </div>
+      <div class="container">
+      <div class="formbold-form-wrapper" ref="formWrapper">
+        <form @submit.prevent="createArticle">
+          <div class="formbold-mb-5">
+            <label for="name" class="formbold-form-label">글제목</label>
+            <input type="text" name="name" id="name" placeholder="제목을 입력하세요" class="formbold-form-input"
+              v-model.trim="title" />
+          </div>
+
+          <div class="formbold-mb-5">
+            <label for="message" class="formbold-form-label">내용</label>
+            <textarea rows="12" name="message" id="message" placeholder="내용을 입력하세요" class="formbold-form-input"
+              v-model="content"></textarea>
+          </div>
+
+          <div class='text-center'>
+            <button class="formbold-btn w-full" type="submit" id="submit">작성하기</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </div></div>
   </div>
 </template>
 
 <script>
-import Home01Navbar from '@/components/HOME/Home01Navbar.vue'
-import axios from 'axios'
-const API_URL = 'http://127.0.0.1:8000'
+  import Home01Navbar from '@/components/HOME/Home01Navbar.vue'
+  import axios from 'axios'
+  const API_URL = 'http://127.0.0.1:8000'
 
-export default {
-  name: 'CreateArticleView',
-  components: {
-    Home01Navbar,
-  },
-  data() {
-    return {
-      title: null,
-      content: null,
-    }
-  },
-  methods: {
-    createArticle() {
-      const title = this.title
-      const content = this.content
-
-      if (!title) {
-        alert('제목 입력해주세요')
-        return
-      } else if (!content){
-        alert('내용 입력해주세요')
-        return
+  export default {
+    name: 'CreateArticleView',
+    components: {
+      Home01Navbar,
+    },
+    data() {
+      return {
+        title: null,
+        content: null,
       }
-      axios({
-        method: 'post',
-        url: `${API_URL}/community/`,
-        data: { title, content },
-        headers: {
-          'Authorization' : `Token ${this.$store.state.auth.token}`
+    },
+    methods: {
+      createArticle() {
+        const title = this.title
+        const content = this.content
+
+        if (!title) {
+          alert('제목을 입력해주세요')
+          return
+        } else if (!content) {
+          alert('내용을 입력해주세요')
+          return
         }
-      })
-      .then(() => {
-        this.$router.push({name: 'CommunityView'})
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+        axios({
+          method: 'post',
+          url: `${API_URL}/community/`,
+          data: { title, content },
+          headers: {
+            'Authorization': `Token ${this.$store.state.auth.token}`
+          }
+        })
+          .then(() => {
+            this.$router.push({ name: 'CommunityView' })
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      }
     }
   }
-}
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  body {
+    font-family: "Inter", sans-serif;
+  }
+
+  .form-container {
+    width: 90%;
+    margin: 30px auto;
+  }
+
+  .first{
+    margin-top: 50px;
+    margin-bottom: 30px;
+    margin-left: 10px;
+  }
+
+  .formbold-mb-5 {
+    margin-bottom: 30px;
+  }
+
+  .formbold-pt-3 {
+    padding-top: 12px;
+  }
+
+  .formbold-main-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 48px;
+  }
+
+  .formbold-form-wrapper {
+    margin: 0 auto;
+    /* max-width: 550px; */
+    width: 100%;
+    height: 630px;
+    background: #212529;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
+    padding: 10px;
+    /* margin-top: 50px; */
+  }
+
+  .formbold-form-label {
+    display: block;
+    font-weight: 500;
+    font-size: 20px;
+    color: white;
+    margin: 12px;
+  }
+
+  .formbold-form-label-2 {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  .formbold-form-input {
+    width: 100%;
+    padding: 12px 24px;
+    border-radius: 6px;
+    background: #212529;
+    font-weight: 500;
+    font-size: 16px;
+    color: white;
+    outline: none;
+    resize: none;
+    /* border: solid 0.5px #8f8f8f; */
+  }
+
+  .formbold-form-input:focus {
+    border-color: #5b5a74;
+    /* box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05); */
+  }
+
+  .formbold-btn {
+    text-align: center;
+    font-size: 16px;
+    border-radius: 6px;
+    padding: 14px 32px;
+    border: none;
+    font-weight: 600;
+    background-color: #8184a0;
+    color: white;
+    cursor: pointer;
+  }
+
+  .formbold-btn:hover {
+    box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
+  }
+
+  .formbold--mx-3 {
+    margin-left: -12px;
+    margin-right: -12px;
+  }
+
+  .formbold-px-3 {
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .flex-wrap {
+    flex-wrap: wrap;
+  }
+
+  .w-full {
+    width: 20%;
+  }
+
+  .formbold-form-header h3 {
+    font-weight: 700;
+    font-size: 20px;
+    color: white;
+  }
+
+  @media (min-width: 540px) {
+    .sm\:w-half {
+      width: 50%;
+    }
+  }
 </style>
--- front-end/src/views/DetailArticleView.vue
+++ front-end/src/views/DetailArticleView.vue
@@ -1,19 +1,53 @@
 <template>
+
+
+
+
+
   <div class="container">
     <Home01Navbar />
-    <h1>Detail</h1>
-    <p>글 번호 : {{ article?.id }}</p>
-    <p>작성자 : {{ article?.user.username }}</p>
-    <p>제목 : {{ article?.title }}</p>
-    <p>내용 : {{ article?.content }}</p>
-    <p>작성시간 : {{ article?.created_at }}</p>
-    <p>수정시간 : {{ article?.updated_at }}</p>
+    <div class='article-container'>
+      <h3 class='ml-55 mt-50 mb-20'><i class='fa fa-film'></i> 영화수다</h3>
+      <div class="ml-55 article-box">
+
+
+
+    <!-- <h1>Detail</h1> -->
+    <div class='title'>{{ article?.title }}</div>
+      <div>
+    <!-- <p>글 번호 : {{ article?.id }}</p> -->
+
+      <div class='title-user'>
+        <span class='title-date2'>
+        <strong style="color:white; margin-left:5px;">{{ article?.user.username }}</strong>
+        </span>
+        <span class='title-date'>
+          <strong style='color:white; margin-left:7px;'>{{formatDate(article?.created_at) }}</strong>
+        </span> 
+      </div>
+<!-- <div class='title-user'>작성시간 : </div> -->
+      </div>
+    <hr class='my-hr'>
+
+    <p class='content'>{{ article?.content }}</p>
+    <!-- <p>작성시간 : {{ article?.created_at }}</p>
+    <p>수정시간 : {{ article?.updated_at }}</p> -->
+    </div>
+    
     <div v-if="article?.user.username === userName">
       <button @click="moveToUpdate">수정</button>
       <button @click="deleteArticle">삭제</button>
     </div>
-    <Community03CreateComment/>
+
+
+
+
+
+
+    </div>
+
     <Community03CommentList/>
+    <Community03CreateComment/>
   </div>
 </template>
 
@@ -45,6 +79,13 @@ export default {
     this.getArticleDetail()
   },
   methods: {
+  formatDate(date) {
+    if (date) {
+      const options = { month: 'short', day: 'numeric' };
+      return new Date(date).toLocaleDateString('ko-KR', options);
+    }
+    return '';
+  },
     getArticleDetail() {
       axios({
         method: 'get',
@@ -81,3 +122,61 @@ export default {
   }
 }
 </script>
+
+
+<style scoped>
+
+.my-hr{
+  margin-top: 7px;
+  border: 0;
+  height: 2px;
+  background-color: white;
+}
+
+.mb-20{
+  margin-bottom: 20px;
+}
+
+.article-box{
+  height: 500px;
+  width: 88%;
+  background-color: rgb(33, 33, 37);
+  padding:15px;
+  border-radius: 1%;
+  margin: 0 auto;
+  
+}
+
+.title{
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-size: 30px;
+  font-weight: 500;
+}
+
+.title-user{
+  font-size: 22px;
+  /* margin-left:  */
+  /* font-weight: bold; */
+  /* margin-left: 10px; */
+}
+
+
+.title-date{
+  font-size:15px;
+  color:whitesmoke
+}
+.title-date2{
+  font-size:20px;
+  color:whitesmoke
+}
+.content{
+  font-size: 20px;
+  margin-top: 15px;
+}
+
+
+
+
+
+</style>
